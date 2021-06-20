cf8e96b5dc441c4b3c80615f360362dce45e3720
egglogger <86017565+egglogger@users.noreply.github.com>
2021-06-19 19:24:42 -0500

    Delete EL2_01 directory

--- EL2_01/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains all onboard and data-extraction code in an uncompiled state. MATLAB is required to extract stored data. 
--- EL2_01/ff9/src/pic24f.h
+++
@@ -1,3 +0,0 @@
-/* Macros for enabling and disabling interrupts (for critical code sections) */
-#define _DI()		__asm__ volatile("disi #0x3FFF")
-#define _EI()		__asm__ volatile("disi #0")
